787458f260ef552caca8f7295236b19fc5c929be
Ryan Will <ryan_will-@hotmail.com>
2025-12-18 10:49:19 -0300

    hotfix: add exportation itens const to auto set

--- src/components/Corpo/index.ts
+++ src/components/Corpo/index.ts
@@ -1,12 +1,18 @@
-import './Partes/styles.css';
+import "./Partes/styles.css";
 
-export { default as CorpoHumano } from './CorpoHumano';
+export { default as CorpoHumano } from "./CorpoHumano";
 export type {
   TPartesCostas,
   TPartesFrente,
   TPartesSelecionadasCostas,
   TPartesSelecionadasFrente,
   TPartesSelecionadasProps,
-} from './CorpoHumano';
-export { default as CorpoHumanoCostas } from './Partes/CorpoHumanoCostas';
-export { default as CorpoHumanoFrente } from './Partes/CorpoHumanoFrente';
+} from "./CorpoHumano";
+export {
+  default as CorpoHumanoCostas,
+  partesCorpoCostas,
+} from "./Partes/CorpoHumanoCostas";
+export {
+  default as CorpoHumanoFrente,
+  partesCorpoFrente,
+} from "./Partes/CorpoHumanoFrente";
